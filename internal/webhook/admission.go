bc90f59d5b2f0a9ba3f96c388c5e9951761e4f69
Rajat Jindal <rajatjindal83@gmail.com>
2024-02-20 12:25:20 +0530

    wait for webhook to be ready
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- internal/webhook/admission.go
+++ internal/webhook/admission.go
@@ -21,7 +21,7 @@ func LazyWebhookStarter(mgr ctrl.Manager) error {
 		case <-ticker.C:
 			_, err := os.ReadFile(crtFile)
 			if err != nil && os.IsNotExist(err) {
-				webhookSetupLog.Info("file does not exist yet\n", "file", crtFile)
+				webhookSetupLog.Info("file does not exist yet")
 				continue
 			}
 
